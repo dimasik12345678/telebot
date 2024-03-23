4fa5e9c3549142b87a586a6d8a5256b38408ed1b
dimasik12345678 <162459293+dimasik12345678@users.noreply.github.com>
2024-03-23 21:25:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,8 @@
-import requests
-import json
-from config import keys
+TOKEN = 'это мой, у вас есть свой)'
 
-class ConvertionExceprion(Exception):
-    pass
+keys = {
+       'Доллар':'USD',
+       'Евро':'EUR',
+        'Рубль':'RUB',
+}
 
-class CryptoConverter:
-    @staticmethod
-    def convert(quoit: str, base: str, amount: float,):
-        if quoit == base:
-            raise ConvertionExceprion(f'Невозможно перевести одинаковые валюты {base}.')
-        try:
-            quoit_ticker = keys[quoit]
-        except KeyError:
-            raise ConvertionExceprion(f'Не удалось обработать валюту {quoit}.')
-        try:
-            base_ticker = keys[base]
-        except KeyError:
-            raise ConvertionExceprion(f'Не удалось обработать валюту {base}.')
-        try:
-            amount = float(amount)
-        except ValueError:
-            raise ConvertionExceprion(f'Не удалось обработать количество {amount}.')
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quoit_ticker}&tsyms={base_ticker}')
-        total_base = json.loads(r.content)[keys[base]]*float(amount)
-        return total_base
